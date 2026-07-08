491dbdbf27c2cf4cb0c07d3983045de15a164e77
MSubham06 <msubham246@gmail.com>
2026-07-08 19:22:09 +0530

    Clean repo: remove build artifacts, editor configs, fix folder naming

---
+++ M18/app.js
@@ -0,0 +1,197 @@
+// console.log("Hello World!");
+// console.log("Apna College");
+// let a = 10;
+// let b = 5;
+// console.log("Sum is",a+b);
+// console.log("Sum of",a,"+",b, "is", a+b);
+
+// let pencilprice = 10;
+// let erasorprice = 5;
+// let output = "The total price is " + (pencilprice + erasorprice) + " Rupees.";
+// let output = `The total price is ${pencilprice + erasorprice} Rupees.`
+// console.log(output);
+
+// let age = 13;
+// if (age>=18) {
+//     console.log("You can Vote.")
+// } else if {
+//     console.log("You can't Vote.")
+// } else {
+//     console.log("You can't Vote.")
+// }
+
+// let color = "red";
+// if (color==="red") {
+//     console.log("Stop!");
+// }
+// if (color==="yellow") {
+//     console.log("Slow Down!");
+// }
+// if (color==="green") {
+//     console.log("Gooo!");
+// }
+
+// let age = 13;
+// if (age>=18) {
+//     console.log("You can apply for Licence.")
+// } else {
+//     console.log("You can't apply for Licence.")
+// }
+
+
+// let color = "ressd";
+// if (color==="red") {
+//     console.log("Stop!");
+// } else if (color==="yellow") {
+//     console.log("Slow Down!");
+// } else if (color==="green") {
+//     console.log("Gooo!");
+// } else {
+//     console.log("Traffic Light Is not working be caution!");
+// }
+
+// Practice Question
+// let size = 'L'; //Options are XL, L, M, S
+// if (size === 'XL') {
+//     console.log("Price is Rs. 250");
+// } else if (size === 'L') {
+//     console.log("Price is Rs. 200");
+// } else if (size === 'M') {
+//     console.log("Price is Rs. 100");
+// } else if (size === 'S') {
+//     console.log("Price is Rs. 50");
+// } else {
+//     console.log("Select the Correct Pack!");
+// } 
+
+
+
+// let mark = 75;
+// if (mark>=33){
+//     if (mark>= 80) {
+//         console.log("Topper Grade!")
+//     } else {
+//         console.log("Avg Grade!")
+//     }
+// } else {
+//     console.log("Better Luck Next Time!")
+// }
+
+// let str = "apple";    // Output: Good String!
+// let str = "Apple";    // Output: Bad String!
+// let str = "app";    // Output: Bad String!
+// let str = "Mango";    // Output: Bad String!
+// if ( str[0]==="a" && str.length>3) {
+//     console.log("Good String!")
+// } else {
+//     console.log("Bad String!")
+// }
+
+
+
+// let num = 12;
+// if ((num%3 === 0) && ((num+1 == 15) || (num-1 == 11))) {
+//     console.log("Safe");
+// } else {
+//     console.log("Unsafe");
+// }
+
+// let color = "greends";
+// switch(color) {
+//     case "red" :
+//         console.log("Stop!");
+//         break;
+//     case "yellow" :
+//         console.log("Slow down");
+//         break;
+//     case "green" :
+//         console.log("Go");
+//         break;
+//     default :
+//         console.log("Broken Switch!")
+// }
+
+
+
+// alert("Something is Wrong!")
+// console.log("This is a Error!");
+// console.error("This is a Error!");
+// console.warn("This is a Error!");
+
+
+// prompt("Please Enter your Roll No.")
+// let name = prompt("Please Enter your Name below!")
+// alert("Welcome " + name);
+// console.log("Welcome " + name);
+
+
+// Assignment Question
+
+// Q1
+// console.log("Question no. 01");
+// let num = 15;
+// if ((num%10)==0) {
+//     console.log("Good!");
+// } else {
+//     console.log("Bad!");
+// }
+
+// Q2
+// console.log("Question no. 02");
+// let name = prompt("Enter your name here!");
+// let age = prompt("Enter your age now!");
+// alert(`${name} is ${age} years old.`);
+
+// Q3
+// console.log("Question no. 03");
+// let Quarter = 2
+// switch(Quarter) {
+//     case 1 : 
+//         console.log("January, February,March");
+//         break;
+//     case 2 : 
+//         console.log("April, May, June");
+//         break;
+//     case 3 : 
+//         console.log("July, August, September");
+//         break;
+//     case 4 : 
+//         console.log("October, November, December");
+//         break;
+//     default : 
+//         console.log("Enter the Quarter Number between 1 to 4 only!");
+//         break;
+// }
+
+// Q4
+// console.log("Question no. 04");
+// let str = "Apple";
+// if (str[0]=="a" || str[0]=="A" && str.length>=5) {
+//     console.log("It's a Golden String!");
+// } else {
+//     console.log("It's Not a Golden String!");
+// }
+
+// Q5
+// console.log("Question no. 05");
+// let a = 2;
+// let b = 4;
+// let c = 1;
+
+// if (a>b && a>c) {
+//     console.log(`The Largest number is ${a}`);
+// } else if (b>a && b>c) {
+//     console.log(`The Largest number is ${b}`);
+// } else {
+//     console.log(`The Largest number is ${c}`);
+// }
+
+// Q6
+// console.log("Question no. 06");
+// let num1 = 32;
+// let num2 = 47852;
+// if ((num1%10)==(num2%10)) {
+//     console.log("Last Number is same in both the Numbers!");
+// } else {
+//     console.log("Last Number is not same in both the Numbers!");
+// }
---
+++ M19/app.js
@@ -0,0 +1,47 @@
+// let pass = prompt("Enter your password");
+// let finalpass = pass.trim();
+// console.log(finalpasspass);
+
+// let students = ["Subham", "Amarleen", "Alive"]
+
+
+// let primary = ['red', 'yellow', 'blue']
+// let secondary = ['orange', 'green', 'violet']
+
+// let color = ['red', 'yellow', 'blue', 'orange', 'green', 'violet']
+
+
+// let color = ['red', 'yellow', 'blue', 'orange', 'green', 'violet']
+
+// let days = ["monday", "sunday","wednesday", "tuesday"];
+// let num = [2,32,3,23,4,46,78,56];
+
+// let str = prompt("Please enter a string");
+// if (str.length == 0 ) {
+//     console.log("String is Empty!");
+// }else {
+//     console.log("String is Not Empty!");
+// }
+
+// let str = "ApNaCoLlEgE";
+// let idx = 3;
+// if (str[idx] == str[idx].toLowerCase() ) {
+//     console.log("Character is Lowercase!");
+// }else {
+//     console.log("Character is Not a Lowercase");
+// }
+
+
+// let str = prompt("Please enter a string");
+// console.log(`Orginal String : ${str}`);
+// console.log(`OString without space : ${str.trim()}`);
+
+
+
+let arr = ["hello",'a',23,64,99,-6];
+let item = 64;
+if (arr.indexOf(item) != -1 ) {
+    console.log("element exists in array");
+}else {
+    console.log("element doesn't exist in array");
+}
