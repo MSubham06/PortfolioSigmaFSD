3c4654d8a3ec19f55973ec3eb1aad5e7beca2313
MSubham06 <msubham246@gmail.com>
2026-07-13 13:05:44 +0530

    Add top scroll fade gradient, fix navbar mobile wrap and Safari compat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   return (
     <div className="main container1">
       <Loader />
+      <div className="top-fade"></div>
       <Navbar />
       <Home />
       <StatsStrip />
